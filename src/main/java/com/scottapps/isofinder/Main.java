8b55d9387b31a117fe5ae8c3b9d40b8c56e95168
Scott <47502831+cyberProjects@users.noreply.github.com>
2023-06-27 22:20:45 -0400

    Revert "Permutationlist"

--- src/main/java/com/scottapps/isofinder/Main.java
+++ src/main/java/com/scottapps/isofinder/Main.java
@@ -1,21 +1,14 @@
 package com.scottapps.isofinder;
 
-import com.scottapps.isofinder.model.DeviceImpl;
+import com.scottapps.isofinder.model.Device;
 
 public class Main {
 
     public static void main(String[] args) {
-        // SIMULATION 1
-        var d1 = new DeviceImpl(8L);
-        d1.initialize();
-        var d2 = new DeviceImpl(8L);
-        d2.initialize();
-        // Because the devices are not bootstrapped with
-        // previously generated graphs, in this simulation
-        // device 2 doesn't know devices 1's public graphs.
-        // as such, most testing was done by manually observing
-        // graphs after the permutation was applied on the verifier's side
-        var result = d2.accept(d1.prove(d2.verify(d1.getH())));
-        System.out.println("Result of attestation round: " + result);
+        var device1 = new Device("SomeSecret2");
+        System.out.println(Device.bytesToHex(device1.getG1()));
+        System.out.println(Device.bytesToHex(device1.getG2()));
+        System.out.println(device1.getPi());
+        System.out.println(Device.bytesToHex(device1.applyInversePi(device1.getG2())));
     }
 }
